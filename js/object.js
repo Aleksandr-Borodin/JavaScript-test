cd5490093904d84f5d654f2e08b6bd8e49f95cba
Aleksandr-Borodin <aleksandrov477@gmail.com>
2019-08-20 18:43:50 +0400

    Наследование от прототипов Общие св. в объектов класса через использование общего прототипа

--- js/object.js
+++ js/object.js
@@ -23,4 +23,18 @@ Object.defineProperty(d, "name", {
   // alert(a.name);
   // alert(b.name);
   // alert(c.name);
-alert(d.name);
+//alert(d.name);
+
+// Прототипы, наследование
+function A1() {
+  this.colors = ["grey", "yellow"];
+}
+function A2 () {}
+A2.prototype = new A1();
+//A2.prototype = {};	// затрется наследование, если попытаться добавить св. через литерал;
+
+var d = new A2();
+d.colors.push("red", "black");	// изменения коснутся и других об. кл., поскольку массив colors принадлежит прототипу A1;
+
+var c = new A2();
+alert(c.colors);
