a5cd451a4a2f89b5e30908ae7acd37f3493854a8
Aleksandr-Borodin <aleksandrov477@gmail.com>
2019-09-07 21:54:55 +0400

    Только базовые возможности по работе с объектами Геттеры и сеттеры Задание атрибутов свойств через литерал

--- js/object.js
+++ js/object.js
@@ -1,85 +1,45 @@
-// alert("Hello World");
-  
 var a = new Object();
 a.name = "marko";
-a.age = 29;
- 
+a.age = 29; 
 var b = {};
 b.name = "polo";
-b.age = 28;
-  
+b.age = 28;  
 var c = {
   name: "jamie",
   age: 30
 }
-var d = {};
-d.name = "Дмитрий";
-Object.defineProperty(d, "name", {
-  value: "Вася",
-  writable: false, // запретить присвоение "user.name="
-  configurable: false // запретить удаление "delete user.name"
-});
-  //Object.defineProperty(b, "name" {value: "Вася", configurable: true, writable: true, enumerable: true });
-  // alert(a.name);
-  // alert(b.name);
-  // alert(c.name);
-//alert(d.name);
-
-// Прототипы, наследование
-function A1() {
-  this.colors = ["grey", "yellow"];
-}
-function A2 () {}
-A2.prototype = new A1();
-//A2.prototype = {};	// затрется наследование, если попытаться добавить св. через литерал;
-
-var d = new A2();
-d.colors.push("red", "black");	// изменения коснутся и других об. кл., поскольку массив colors принадлежит прототипу A1;
-
-var c = new A2();
-//alert(c.colors);
-
-// Использование паттерна Кража конструктора для исправления общего исопльзования св. протипа объектами класса
-function B1() {
-  this.colors = ["t1", "t2"];
-}
-function B2 () {
-  B1.call(this);
-}
-
-var dub = new B2();
-dub.colors.push("t3", "t4");
-
-var dub2 = new B2();
-//alert(dub.colors);
-//alert(dub2.colors);
-
-
-// Комбинированное наследование
-function D1(name) {
-  this.name = name;
-  this.colors = ["d1", "d2"];
-}
-D1.prototype.SayName = function() {
-  alert(this.name);
+var d = {
+  Orbit: function(){
+    return this.name;
+  },
+  get fullname(){
+    return this.name + " name";
+  }
 };
+d.name = "Дмитрий";
+d.secondName = "Маша";
 
-function D2 (name) {
-  D1.call(this, name);	// наследование свойств
-}
-D2.prototype = new D1();
+Object.defineProperty(d, "name", {
+  get: function(){	
+    return this.secondName;
+  }
+});
+//alert(d.name);
+//alert(d.Orbit());
+//alert(d.fullname);
+//alert(Object.key(d));
 
-var hub = new D2("Sasha");
-hub.colors.push("d3", "d4");
-alert(hub.colors);
+// Узнать доступные св. и методы в объекта класса;
+//alert(Object.keys(d));
 
-var hub2 = new D2("Jamie");
-hub2.SayName();
-alert(hub2.colors);
+// Задание сразу нескольких свойств;
+var d2 = {};
+Object.defineProperties(
+    d2,
+    {
+        name:{ enumerable: true, configurable: true, get: function(){return this.secondname + ' имя';}},
+        secondname:{ value : "Дима", writable: true, enumerable: true, configurable: true}
+    }
+);
 
-
-
-
-
-
-
+alert(d2.name);
