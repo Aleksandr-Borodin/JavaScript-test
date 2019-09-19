05572c64360098155ef2430aceb16553b6cd445d
Aleksandr-Borodin <aleksandrov477@gmail.com>
2019-09-19 20:56:53 +0400

    Об.: windows, screen, navigator, history, location, всплывающие окна

---
+++ js/bom.js
@@ -0,0 +1,33 @@
+// Алерты)
+//confirm("Есть ли жизнь на Марсе?");
+var a = prompt("Вопрос?", "Ответ по умолчанию");
+
+// Выполнение с задержкой
+setTimeout(function(){alert("Пользователь ввел ответ: " + a);}, 2000);
+
+
+// Об. screen
+//alert(screen.availWidth);
+
+// Об. history
+//history.back();
+
+// Об. navigator
+//alert(navigator.userAgent);
+//alert(navigator.language);
+
+// Об. location
+//alert(location.href);
+//alert(location.host);
+//alert(location.pathname);
+//alert(location.protocol);
+//location.href = "https://www.google.com.ua/";
+//location.replace("https://www.google.com.ua/");
+
+// Об. windows
+// Сработает, если это произошло в результате действий пользователя
+//windows.open("https://www.google.com.ua/", "_self");
+// Сработает, если окно является результатом предыдущей ф-ции
+//windows.resizeTo(800, 600);
+
+
