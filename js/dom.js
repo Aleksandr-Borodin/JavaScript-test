458d9b469b191cb596c07635e914a8b173eb88e6
Aleksandr-Borodin <aleksandrov477@gmail.com>
2019-09-23 15:57:47 +0400

    Чтение древа тегов Запись нового тега

--- js/dom.js
+++ js/dom.js
@@ -7,8 +7,16 @@ var d1 = document.getElementById("bla");
 //d1.childNodes[0].appendData(" Новый текст для 0-го абзаца");
 //d1.childNodes[0].insertData(3, " Новый текст для 0-го абзаца");
 
+// Чтение тега и его значению. Иследование древа тегов.
 var d2 = document.getElementById("bla2");
 //alert(d2.childNodes[0].nodeType);
 alert(d2.childNodes[0].childNodes[0].nodeValue);
+
+// Вставка нового тега
+var eldiv = document.createElement("div");
+eldiv.className = "jeam";
+var textmode = document.createTextNode("текст внутри тегов");
+eldiv.appendChild(textmode);
+document.body.appendChild(eldiv);
 
 
