432be6934b0b5d057907ee45e37c5a73125e5c93
Aleksandr-Borodin <aleksandrov477@gmail.com>
2020-01-23 22:55:14 +0300

    Вставка html-кода

--- js/dom.js
+++ js/dom.js
@@ -9,8 +9,10 @@ var d1 = document.getElementById("bla");
 
 // Чтение тега и его значению. Иследование древа тегов.
 var d2 = document.getElementById("bla2");
+
 //alert(d2.childNodes[0].nodeType);
-alert(d2.childNodes[0].childNodes[0].nodeValue);
+
+//alert(d2.childNodes[0].childNodes[0].nodeValue);
 
 // Вставка нового тега
 var eldiv = document.createElement("div");
@@ -18,5 +20,13 @@ eldiv.className = "jeam";
 var textmode = document.createTextNode("текст внутри тегов");
 eldiv.appendChild(textmode);
 document.body.appendChild(eldiv);
+
+// Вставка в конкретное место
+d2.insertAdjacentHTML('beforebegin', "<b>текст</b>");
+
+var d3 = document.getElementById("blabla");
+var d4 = document.getElementById("blabla2");
+// Прокрутка
+//d3.scrollIntoView();
 
 
